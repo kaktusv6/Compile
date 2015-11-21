dae8f764e3d11347e5c0f2a908784d28b746ae9d
kaktusv6 <kaktusv6@gmail.com>
2015-11-21 14:49:16 +0400

    *

--- Компилятор/Компилятор/Token.h
+++ Компилятор/Компилятор/Token.h
@@ -1,13 +1,25 @@
 #pragma once
+#include <iostream>
 #include <string>
+#include <map>
+
+using namespace std;
+
 class Token
 {
 private:
-	enum tokenKeywords { BEGIN, FORWARD, DO, ELSE, END, FOR, FUNCTION, IF, ARRAY, OF, PROCEDURE, PROGRAM, RECORD, THEN, TO, TYPE, VAR, WHILE, BREAK, CONTINUE, DOWNTO, EXIT, REPEAT, UNTIL };
+	enum token {ID, OP, BAD, BEGIN, FORWARD, DO, ELSE, END, FOR, FUNCTION, IF, ARRAY, OF, PROCEDURE, PROGRAM, RECORD, THEN, TO, TYPE, VAR, WHILE, BREAK, CONTINUE, DOWNTO, EXIT, REPEAT, UNTIL };
+	token tok;
+	
+	map <string, token> keywordsMap;
+	
+	//string stringToken;
 	//string tokenOperation[25] = { "and", "div", "mod", "not", "or", "xor", "+", "-", "*", "/", "^", "+=", "-=", "*=", "/=", "<", ">", "<=", ">=", "=", "<", ">", ":=", "@", "."};
-public:
-	Token();
-	~Token();
 
+public:
+	Token(); // конструктор класса
+
+	void lexemToToken(string s);
+	string tokenToString();
 };
 
