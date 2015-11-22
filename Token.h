87b839ee2db1aad54278d7751c53775ceaf1868b
kaktusv6 <kaktusv6@gmail.com>
2015-11-22 15:56:07 +0400

    Изменил класс Toke тип токена enum смерил на string Закоментировал строки кода где сласс работал с токенами типа enum

--- Token.h
+++ Token.h
@@ -5,14 +5,20 @@
 
 using namespace std;
 
+const int amountKeywords = 24;
+
 class Token
 {
 private:
-	enum token {ID, OP, BAD, BEGIN, FORWARD, DO, ELSE, END, FOR, FUNCTION, IF, ARRAY, OF, PROCEDURE, PROGRAM, RECORD, THEN, TO, TYPE, VAR, WHILE, BREAK, CONTINUE, DOWNTO, EXIT, REPEAT, UNTIL };
-	token tok;
-	
-	map <string, token> keywordsMap;
-	
+	//enum token {ID, OP, BEGIN, FORWARD, DO, ELSE, END, FOR, FUNCTION, IF, ARRAY, OF, PROCEDURE, PROGRAM, RECORD, THEN, TO, TYPE, VAR, WHILE, BREAK, CONTINUE, DOWNTO, EXIT, REPEAT, UNTIL };
+	//token tok;
+	string token;
+
+	//map <string, token> keywordsMap;
+
+	string keywords[amountKeywords] = {
+		 "begin", "forward", "do", "else", "end", "for", "function", "if", "array", "of", "procedure", "program", "record", "then", "to", "type", "var", "while", "break", "continue", "downto", "exit", "repeat", "until
+	};
 	//string stringToken;
 	//string tokenOperation[25] = { "and", "div", "mod", "not", "or", "xor", "+", "-", "*", "/", "^", "+=", "-=", "*=", "/=", "<", ">", "<=", ">=", "=", "<", ">", ":=", "@", "."};
 
