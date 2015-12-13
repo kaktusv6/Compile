05c9411dc5b2b76225c41c017accb6ceba7f3df9
kaktusv6 <kaktusv6@gmail.com>
2015-12-13 22:45:02 +0400

    Избавился отшаблона

--- Token.cpp
+++ Token.cpp
@@ -21,16 +21,23 @@ void Token::printToken()
 	token = _token;
 	lexText = _lexText;
 }*/
-
-template<typename T>
-void TokenValue<typename T>::printToken()
+/*void TokenValue<T>::printToken()
 {
-	fout << lexLine << '\t'
+	fout << this->lexLine << '\t'
+		<< lexCol << '\t' 
+		<< token << '\t'
+		<< lexText << '\t' 
+		<< valueToken << '\n';
+}*/
+void TokenValue::printToken()
+{
+	fout <<lexLine << '\t'
 		<< lexCol << '\t'
 		<< token << '\t'
 		<< lexText << '\t'
 		<< valueToken << '\n';
 }
+
 void TokenError::printToken()
 {
 	fout << lexLine << '\t'
