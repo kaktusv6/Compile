973d71c5fd54ac7bb65aee525d83c6415af74086
kaktusv6 <kaktusv6@gmail.com>
2015-12-17 21:55:59 +0400

    Переименовал Лексический анализатор.cpp на main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include <fstream>
-#include "Lexem.h"
-#include "Token.h"
 #include "Lexer.h"
 
 int main()
 {
 	initMaps();
 	Lexer l;
-	while (l.endFile && !fin.eof())
+	while (!l.endFile && !fin.eof())
 	{
 		l.nextLexem();
 	}
