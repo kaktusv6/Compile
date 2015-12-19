fd9518333bb869b13104847c83d46e0dfe010275
kaktusv6 <kaktusv6@gmail.com>
2015-12-19 22:20:04 +0400

    Разрешил конфликт последнего слияния: main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+
+#include "Lexer.h"
+int main()
+{
+	Lexer *l = new Lexer();
+	Token *t;
+	while (!l->getEndFile())
+	{
+		t = l->nextToken();
+		if (t != NULL) { t->printToken(); }
+	}
+	return 0;
+}
