0ae201b51464f8eba1c5bc83a0dc435ed9a46ed2
kaktusv6 <kaktusv6@gmail.com>
2016-01-05 00:32:44 +0400

    Стер старое определение метода takeToken не подходило по теории синтаксера.

--- Syntax.cpp
+++ Syntax.cpp
@@ -7,32 +7,5 @@ Syntax::Syntax()
 
 void Syntax::takeToken(Token *t)
 {
-	Node *n = new Node(t);
 
-	if (t->getToken() == "integer")
-	{
-		if (node->getExpression() == NULL)
-		{
-			//node->setLeft(n);
-		}
-		else
-		if (node->getExpression()->getToken() == "op")
-		{
-			//node->setRight(n);
-		}
-
-	}
-	else if (t->getToken() == "op")
-	{
-		if (node->getExpression() == NULL)
-		{
-			node->setExpression(t);
-		}
-		else if (node->getExpression()->getToken() == "op")
-		{
-			//n->setLeft(node);
-			node->setParent(n);
-			node = n;
-		}
-	}
 }
