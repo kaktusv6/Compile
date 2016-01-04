c13dcfaefa589587b3dbc8bdd8c6a70ac1cbde2f
kaktusv6 <kaktusv6@gmail.com>
2016-01-05 00:37:40 +0400

    Закоментировал модуль Syntax пока не напишу нормальный класс Node.

--- main.cpp
+++ main.cpp
@@ -1,23 +1,23 @@
 #include "Lexer.h"
-#include "Syntax.h"
+//#include "Syntax.h"
 
 int main()
 {
 	Lexer *l = new Lexer();
 	Token *t;
-	Syntax *s = new Syntax();
+	//Syntax *s = new Syntax();
 	while (!l->getEndFile())
 	{
 		t = l->nextToken();
 		if (t != NULL)
 		{
-			s->takeToken(t);
+			//s->takeToken(t);
 			
 			t->printToken();
 		}
 		if (l->getBuffer() != NULL)
 		{
-			s->takeToken(l->getBuffer());
+			//s->takeToken(l->getBuffer());
 
 			l->getBuffer()->printToken();
 			l->setBuffer(NULL);
