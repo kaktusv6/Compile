ce18c7d03ed91048e1ed92d76227d66d24570548
kaktusv6 <kaktusv6@gmail.com>
2016-01-04 17:05:34 +0400

    Описал свойства и методы класса Node.

--- Node.h
+++ Node.h
@@ -9,11 +9,29 @@ using namespace std;
 class Node
 {
 private:
-	Token expression;
-	Node *rightChild;
-	Node *leftChild;
+	Token *expression;
+	Node *left;
+	Node *right;
+	Node *parent;
+
 public:
-	Node();
+	Node() : expression(NULL), left(NULL), right(NULL), parent(NULL)
+	{}
+
+	Node(Token *t) : left(NULL), right(NULL), parent(NULL)
+	{
+		expression = t;
+	}
+
+	void setExpression(Token* t) { this->expression = t; }
+	void setLeft  (Node *n) { this->left = n; }
+	void setRight (Node *n) { this->right = n; }
+	void setParent(Node *n) { this->parent = n; }
+
+	Token* getExpression() { return this->expression; }
+	Node* getLeft() { return this->left; }
+	Node* getRight() { return this->right; }
+	Node* getParent() { return this->parent; }
 };
 
 #endif // NODE
