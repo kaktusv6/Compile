1b405d43ac8a93a4252383622722d048f368eeec
kaktusv6 <kaktusv6@gmail.com>
2016-01-04 17:11:59 +0400

    В основном файле после считывания Лексемы Token сразу передается Syntax и после записывается в txt файл.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,23 @@
 #include "Lexer.h"
-
+#include "Syntax.h"
 int main()
 {
 	Lexer *l = new Lexer();
 	Token *t;
+	Syntax *s = new Syntax();
 	while (!l->getEndFile())
 	{
 		t = l->nextToken();
-		if (t != NULL) { t->printToken(); }
+		if (t != NULL)
+		{
+			s->takeToken(t);
+			
+			t->printToken();
+		}
 		if (l->getBuffer() != NULL)
 		{
+			s->takeToken(l->getBuffer());
+
 			l->getBuffer()->printToken();
 			l->setBuffer(NULL);
 		}
