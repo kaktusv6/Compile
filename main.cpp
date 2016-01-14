7a5b88a64cbdda4aaeec50b7545a2c537aa4011f
kaktusv6 <kaktusv6@gmail.com>
2016-01-14 13:01:57 +0400

    В main.cpp добавил обработку Token'ов из Lexer.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
 #include "Lexer.h"
+#include "Syntax.h"
 
 int main()
 {
 	Lexer *l = new Lexer();
 	Token *t;
+	Syntax *s = new Syntax();
 	while (!l->getEndFile())
 	{
 		t = l->nextToken();
-		if (t != NULL) { t->printToken(); }
+		if (t != NULL)
+		{
+			t->printToken();
+			s->takeToken(t);
+		}
 		if (l->getBuffer() != NULL)
 		{
 			l->getBuffer()->printToken();
+			s->takeToken(l->getBuffer());
 			l->setBuffer(NULL);
 		}
 	}
