3e813fa1e2fe55e36680b9dbe810ab85c8318545
kaktusv6 <kaktusv6@gmail.com>
2016-01-15 20:11:02 +0400

    В классе Node объявил методы parsAdd, parsMulti и addPrim. Они строят синтаксическое дерево. Добавил метод creatNode который создает указатель Node.

--- Parser.h
+++ Parser.h
@@ -1,5 +1,5 @@
-#ifndef Parser_H
-#define Parser_H
+#ifndef PARSER_H
+#define PARSER_H
 #include "Lexer.h"
 #include "Node.h"
 
@@ -10,17 +10,33 @@ class Parser
 private:
 	Node* currentNode;
 	Lexer *lexer;
+	Token* t;
+
 	kindNode detKindNode(Node*);
+	kindNode detKindNode(Token*);
+
 	void toChild(Node *parent, Node *child)
 	{
 		child->parent = parent;
 		parent->addChild(child);
 	}
 public:
-	Parser(Lexer *l) : currentNode(NULL), lexer(l)
-	{ }
+	Parser(Lexer *l) : currentNode(NULL), lexer(l), t(NULL)
+	{
+		t = lexer->nextToken();
+	}
 	
+	Node* createNode()
+	{
+		Node* n = new Node(t);
+		t = lexer->nextToken();
+		return n;
+	}
+
 	Node* addNodeInTree(Token*);
+	Node* parsAdd();
+	Node* parsMulti();
+	Node* parsPrim();
 };
 
-#endif // Parser_H
+#endif // PARSER_H
