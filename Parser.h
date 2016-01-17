325d624e250bfcacb8e1d0c3c33edaebe575f894
kaktusv6 <kaktusv6@gmail.com>
2016-01-17 18:34:49 +0400

    Классу SynError добавил конструктор который инициализирует поле err типа kindError добавил метод PrintError.

--- Parser.h
+++ Parser.h
@@ -3,7 +3,7 @@
 #include "Lexer.h"
 #include "Node.h"
 
-enum kindOp {
+enum kindExpr {
 	ADD, MULTI, PRIMER, OPEN_SEP, CLOSE_SEP,
 	RELAT, UNARY, ERR, END_EXPR
 };
@@ -15,7 +15,7 @@ private:
 	Lexer *lexer;
 	Token* t;
 
-	kindOp detKindToken(Token*);
+	kindExpr detKindToken(Token*);
 
 	Token* nextToken()
 	{
@@ -25,11 +25,13 @@ private:
 
 	bool isUnary(Token* tok)
 	{
+		if (t == NULL) return false;
 		string lexText = tok->getLexText();
 		return lexText == "+" || lexText == "-" || lexText == "not";
 	}
 	bool isPrimer(Token* tok)
 	{
+		if (t == NULL) return false;
 		string token = tok->getToken();
 		if (token == "integer" || token == "ident" ||
 			token == "string" || token == "real" || token == "char")
@@ -65,11 +67,31 @@ public:
 	Node* parsRelat();
 };
 
+enum kindError {NO_CLOSE_SEP, NO_PRIMER, NOT_UNARY_EXPR};
+
 class SynError
 {
-public:
-
 private:
-
+	kindError err;
+public:
+	SynError()
+	{}
+	SynError(kindError e) : err(e)
+	{}
+	void printError()
+	{
+		if (err == NO_CLOSE_SEP)
+		{
+			cout << "No closing parenthesis" << endl;
+			system("pause");
+			exit(1);
+		}
+		else if (err == NO_PRIMER)
+		{
+			cout << "No primary-expression" << endl;
+			system("pause");
+			exit(1);
+		}
+	}
 };
 #endif // PARSER_H
