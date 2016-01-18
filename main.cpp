02350191964cd77029a1da5547e6a5122246b2af
kaktusv6 <kaktusv6@gmail.com>
2016-01-18 15:06:29 +0400

    Убрал цикл вызова parsRelat.

--- main.cpp
+++ main.cpp
@@ -4,12 +4,9 @@ int main(int argc, char* argv[])
 {
 	Lexer *l = new Lexer();
 	Parser *p = new Parser(l);
-	Token *t;
 	Node *n = new Node();
-	while (!l->getEndFile())
-	{
-		n = p->parsRelat();
-	}
+
+	n = p->parsRelat();
 	n->printNode(0);
 	system("pause");
 	return 0;
